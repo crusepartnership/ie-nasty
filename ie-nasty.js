cffc4c1f02cdecef2e0a60656003ab91095d2b2e
Tristan Kenney <tristan@kenney.co>
2016-01-19 15:26:58 +1100

    Added trim polyfill

--- ie-nasty.js
+++ ie-nasty.js
@@ -1,9 +1,20 @@
 (function() {
   "use strict";
   //Polyfill for no 'hasOwnProperty' method in IE < 9
+  //Required for ngStorage
   if (window.hasOwnProperty === undefined) {
     window.hasOwnProperty = function(property) {
-      return Object.prototype.hasOwnProperty(this, property);
+      return Object.prototype.hasOwnProperty.call(this, property);
     };
   }
+
+  //Polyfill for native string trim function
+  //Required form math.js
+  if (String.prototype.trim === undefined) {
+    String.prototype = function() {
+      return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    };
+  }
+
+
 })();
