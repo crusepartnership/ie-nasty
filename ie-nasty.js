973d9dcdcd9396298203a4ccc2cde8592bf063fc
Tristan Kenney <tristan@kenney.co>
2016-01-19 15:44:50 +1100

    Removed shims covered by es5-shim

--- ie-nasty.js
+++ ie-nasty.js
@@ -7,14 +7,6 @@
       return Object.prototype.hasOwnProperty.call(this, property);
     };
   }
-
-  //Polyfill for native string trim function
-  //Required form math.js
-  if (String.prototype.trim === undefined) {
-    String.prototype = function() {
-      return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-    };
-  }
-
+ 
 
 })();
